3a89f1ee7b074274429950649b6f5ded07b6f0b3
RamosBits <carydramos218@gmail.com>
2026-07-07 12:07:47 +0800

    Add files via upload

---
+++ lab01/hello_logic.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module hello_logic(
+    input a,
+    input b,
+    output and_y,
+    output or_y,
+    output xor_y,
+    output nand_y,
+    output nor_y
+);
+    assign and_y = a & b;
+    assign or_y  = a | b;
+    assign xor_y = a ^ b;
+    assign nand_y = ~(a & b);
+    assign nor_y  = ~(a | b);
+endmodule
